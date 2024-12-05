86b312b6d5e5acd723d51a44aa325d016aa8756c
zaynom <ouaziz0626@gmail.com>
2024-12-05 10:45:06 +0100

    Add caching to optimize the mongodb connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,12 +4,37 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
-    "Please define the MONGO_URI environment variable in .env.local",
+    "Please define the MONGO_URI environment variable in .env.local"
   );
 }
 
+declare global {
+  let mongoose: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+// @ts-expect-error -- ignore
+let cached = global.mongoose;
+
+if (!cached) {
+  // @ts-expect-error -- ignore
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 async function dbConnect() {
-  return mongoose.connect(MONGODB_URI!).then((mongoose) => mongoose);
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI!, {})
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 export default dbConnect;
---
+++ src/types/global.d.ts
@@ -0,0 +1,6 @@
+declare global {
+  let mongoose: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
