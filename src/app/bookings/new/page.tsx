ba97bdaa0cee27fbb18fdd8782868e01fcb6304f
zaynom <ouaziz0626@gmail.com>
2024-12-05 09:49:41 +0100

    Add date & time selection to the form
    - date picker
    - time slots picker

--- src/app/bookings/new/page.tsx
+++ src/app/bookings/new/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -13,11 +14,16 @@ import {
 } from "@/components/ui/select";
 import { barbers } from "@/data/barbers";
 import { services } from "@/data/services";
+import { timeSlots } from "@/data/time-slots";
+import { FormEvent, useState } from "react";
 
 export default function Page() {
   const [fullname, setFullname] = useState("");
+  const [date, setDate] = useState<Date | undefined>(new Date());
   const [service, setService] = useState("");
   const [barber, setBarber] = useState("");
+  const [selectedTime, setSelectedTime] = useState("");
+
 
   return (
     <div className="py-24">
@@ -37,6 +43,7 @@ export default function Page() {
             <Input
               placeholder="Omar ouaz"
               id="fullname"
+              className="w-[250px]"
               onChange={(e) => setFullname(e.target.value)}
             />
           </div>
@@ -78,6 +85,36 @@ export default function Page() {
               </SelectContent>
             </Select>
           </div>
+
+          <div>
+            <Label htmlFor="date">Date</Label>
+            <Calendar
+              id="date"
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              className="rounded-md border w-fit"
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Select Time
+            </label>
+            <div className="mt-1 grid grid-cols-4 gap-2">
+              {timeSlots.map((time) => (
+                <button
+                  key={time}
+                  type="button"
+                  onClick={() => setSelectedTime(time)}
+                  className={`p-2 text-sm rounded-md ${selectedTime === time ? "bg-gray-900 text-white" : "bg-gray-100 hover:bg-gray-200"}`}
+                >
+                  {time}
+                </button>
+              ))}
+            </div>
+          </div>
+          <Button className="w-[250px]">Book Appointment</Button>
         </form>
       </div>
     </div>
