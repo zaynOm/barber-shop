b27417b60c0642b2472d1481af6c82ae6417ecb6
zaynom <ouaziz0626@gmail.com>
2024-12-05 10:02:42 +0100

    Add utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { format } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatAppointmentDate(date: Date) {
+  return format(date, "EEEE, MMMM dd, yyyy");
+}
+
+export function formatToUsDate(date: Date) {
+  return format(date, "dd-MM-yyyy");
 }
